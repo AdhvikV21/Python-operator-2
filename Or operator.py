fc2165ea41f5ef2ccf18a77054cde4aa50966104
AdhvikV21 <adzydarock@gmail.com>
2025-08-13 18:48:38 +1000

    Add files via upload

---
+++ Or operator.py
@@ -0,0 +1,16 @@
+a = 25
+b = 18
+c = 0
+if a and b and c:
+    print("All the numbers have boolean value as True")
+else:     
+    print ("At least one number has boolean value as False")          
+
+a=10
+b= -10
+c= 0
+if a > 0 or b > 0:
+    print("Either number is greater than 0")
+else:
+    print("Neither number is greater than 0")
+
